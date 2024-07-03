b5da4e51fa0fe57a5fdc319802a4fa1a3f5e0ed8
Chiesa14 <remychiesa14@gmail.com>
2024-07-03 19:31:35 +0200

    Finalizing creating call

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,14 +3,56 @@ import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
 import MeetingModal from "./MeetingModal";
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 
 const MeetingTypeList = () => {
   const router = useRouter();
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: "",
+    link: "",
+  });
+  const [calldetails, setCallDetails] = useState<Call>();
 
-  const createMeeting = () => {};
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+
+  const createMeeting = async () => {
+    if (!client || !user) return;
+    try {
+      const id = crypto.randomUUID();
+
+      const call = client.call("default", id);
+
+      if (!call) return new Error("Failed to create the call");
+
+      const startsAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+
+      const desciption = values.description || "Instant meeting";
+
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            desciption,
+          },
+        },
+      });
+
+      setCallDetails(call);
+
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
@@ -47,7 +89,7 @@ const MeetingTypeList = () => {
         title="Start an Instant Meeting"
         className="text-center"
         buttonText="Start Meeting"
-        handleClick={() => createMeeting}
+        handleClick={createMeeting}
       />
     </section>
   );
