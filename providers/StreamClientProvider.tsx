1e1d7fd51f5c0506a024595b2a4f8fa783f9faf8
Chiesa14 <remychiesa14@gmail.com>
2024-07-12 20:40:37 +0200

    Loader

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -2,6 +2,7 @@ import { tokenProvider } from "@/actions/stream.actions";
 import { useUser } from "@clerk/nextjs";
 import { StreamVideo, StreamVideoClient } from "@stream-io/video-react-sdk";
 import { ReactNode, useEffect, useState } from "react";
+import Loader from "@/components/Loader";
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
@@ -26,8 +27,8 @@ const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
     setVideoClient(client);
   }, [user, isLoaded]);
 
-  if (!videoClient) return;
-  return <StreamVideo client={videoClient}></StreamVideo>;
+  if (!videoClient) return <Loader />;
+  return <StreamVideo client={videoClient}>{children}</StreamVideo>;
 };
 
 export default StreamVideoProvider;
