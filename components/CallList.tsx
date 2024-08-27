2ef77230a1f50eaa9de2d18c8b79af1bf7a0b2be
Chiesa14 <remychiesa14@gmail.com>
2024-08-27 05:33:44 +0200

    call room fixes

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -80,8 +80,8 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
           <MeetingCard
             key={(meeting as Call).id}
             title={
-              (meeting as Call).state?.custom.desciption?.substring(0, 26) ||
-              (meeting as Call).state?.custom.description.substring(0, 26) ||
+              (meeting as Call).state?.custom?.desciption?.substring(0, 26) ||
+              (meeting as Call).state?.custom?.description?.substring(0, 26) ||
               meeting.filename?.substring(0, 20) ||
               "No description"
             }
